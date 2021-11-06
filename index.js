682a3e4cc3e21974bfcb54c22dc886f0de8b3822
PhilGroup <64746978+PhilGroup@users.noreply.github.com>
2021-11-06 14:15:47 -0400

    Asynchrony web part 5

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+//npc.walkEast()
+// npc.stop();
+// npc.walkNorth()
+npc.walkEast(500, () => {
+    npc.walkNorth(500, ()=> {
+        npc.walkSouth(500, ()=>{
+            npc.walkWest(500, );
+        }); 
+    })
+                                                  
+})
 
 // Create the inventory
 const inventory = newInventory()
@@ -20,3 +30,4 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+move();
